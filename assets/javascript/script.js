06744d8333e5c466bd7f1c09eda5b8bf046e2c16
Justin Hoch <justinhoch22@gmail.com>
2021-05-06 22:19:14 -0500

    Added luxon.js to track the time and added an interval to check the time every second

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,19 @@
-let containerEl = $(".container")
-let ulEl = $("<ul>")
-let liEl = []
+var dateTime = luxon.DateTime;
+let dt = dateTime.now();
+let containerEl = $(".container");
+let ulEl = $("<ul>");
+let liEl = [];
 
 var leStorage = {
     "9": "",
     "10": "",
     "11": "",
     "12": "",
-    "1": "",
-    "2": "",
-    "3": "",
-    "4": "",
-    "5": ""
+    "13": "",
+    "14": "",
+    "15": "",
+    "16": "",
+    "17": ""
 }
 
 if (JSON.parse(localStorage.getItem("event")) === null) {
@@ -27,34 +29,46 @@ for (i=0; i<=8; i++) {
     liEl[i].append($("<textarea>"));
     liEl[i].append($("<button>"));
     liEl[i].children().eq(0).text("pog");
-    if (i < 3) {
-        liEl[i].children().eq(0).text(`${(i+9).toString()}AM`);
-        liEl[i].children().eq(2).attr("id", `${(i+9).toString()}`)
-        liEl[i].children().eq(1).val(JSON.parse(localStorage.getItem("event"))[`${(i+9).toString()}`])
-    } else if (i === 3) {
-        liEl[3].children().eq(0).text("12PM")
-        liEl[i].children().eq(2).attr("id", "12")
-        liEl[i].children().eq(1).val(JSON.parse(localStorage.getItem("event"))["12"])
-    } else {
-        liEl[i].children().eq(0).text(`${(i-3).toString()}PM`);
-        liEl[i].children().eq(2).attr("id", `${(i-3).toString()}`);
-        liEl[i].children().eq(1).val(JSON.parse(localStorage.getItem("event"))[`${(i-3).toString()}`])
-    }
-    liEl[i].children().eq(0).css({"margin": "0px", "border-top": "2px solid black", "border-right": "2px solid black", "width": "100px", "height": "100px", "text-align": "right"})
-    liEl[i].children().eq(1).css({"background-color": "#dbdbdb", "width": "700px", "vertical-align": "top", "border": "1px solid black"})
-    liEl[i].children().eq(2).css({"width": "100px", "height": "100px", "background-color": "aqua"})
-    console.log(liEl[i])
+    liEl[i].children().eq(0).text(`${(i+9).toString()}`);
+    liEl[i].children().eq(2).attr("id", `${(i+9).toString()}`);
+    liEl[i].children().eq(1).val(JSON.parse(localStorage.getItem("event"))[`${(i+9).toString()}`]);
+    liEl[i].children().eq(0).css({"margin": "0px", "border-top": "1px solid black", "border-bottom": "1px solid black", "border-right": "2px solid black", "padding-top": "10px", "padding-right": "10px", "width": "100px", "height": "100px", "text-align": "right"});
+    liEl[i].children().eq(1).css({"width": "700px", "height": "100px", "vertical-align": "top", "border": "1px solid black"});
+    liEl[i].children().eq(2).css({"width": "100px", "height": "100px"});
+    liEl[i].children().eq(2).text("💾");
+    liEl[i].children().eq(2).addClass("saveBtn");
     ulEl.append(liEl[i]);
 }
 containerEl.append(ulEl);
 
 function storeText(event) {
     let object = $(event.target);
-    object.siblings().eq(1).val()
+    object.siblings().eq(1).val();
     leStorage[object.attr("id")] = object.siblings().eq(1).val();
-    localStorage.setItem("event", JSON.stringify(leStorage))
+    localStorage.setItem("event", JSON.stringify(leStorage));
 }
 
 for (i in liEl) {
     liEl[i].on("click", "button", storeText);
 }
+
+if (i + 9 < dt.hour) {
+    liEl[i].children().eq(1).css({"background-color": "red"});
+} else if (i + 9 === dt.hour) {
+    liEl[i].children().eq(1).css({"background-color": "green"});
+} else {
+    liEl[i].children().eq(1).css({"background-color": "gray"});
+}
+
+interval = setInterval(function() {
+    for (i in liEl) {
+        dt = dateTime.now();
+        if (parseInt(i) + 9 < dt.hour) {
+            liEl[i].children().eq(1).css({"background-color": "red", "color": "white"});
+        } else if (parseInt(i) + 9 === dt.hour) {
+            liEl[i].children().eq(1).css({"background-color": "green", "color": "white"});
+        } else {
+            liEl[i].children().eq(1).css({"background-color": "#0084ff", "color": "black"});
+        }
+    }
+}, 1000)
